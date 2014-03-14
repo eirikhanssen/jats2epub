6333f657631609306abf0fe79592ed94047d9ec6
Eirik Hanssen <eirikhanssen79@gmail.com>
2014-03-14 19:54:49 +0100

    Update calabash.bat

--- programs/xmlcalabash/calabash.bat
+++ programs/xmlcalabash/calabash.bat
@@ -1,3 +1,4 @@
+@ehco off
 rem for system-wide installation of calabash (able to use the calabash command from any folder),
 rem change this path to reflect the absolute path of calabash.jar and put it in a folder that 
 rem is located in the path environment variable, such as C:\Windows\System32 or any other folder in the path
