d9585372a7983cbdec61872a35ae1d92818762ec
Eirik Hanssen <eirikhanssen79@gmail.com>
2014-03-14 20:06:30 +0100

    Update calabash.bat
    Fix aestethics.

--- calabash.bat
+++ calabash.bat
@@ -3,6 +3,6 @@ rem	Overview:
 rem	
 rem	This is a windows batch file used to run xmlcalabash, an XProc processor
 rem	It is used by the jats2epub.bat windows batch script that should also be
-rem included
+rem	included
 rem
 @java -Xmx1024m -jar "programs\xmlcalabash\calabash.jar" %*
