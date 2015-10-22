9673e89b95557df89c03f07386eedefb55f121ad
Eirik Hanssen <eirikh@hioa.no>
2015-10-22 10:18:08 +0200

    remove xmlcalabash and instead add instructions on how to get the latest version

--- programs/xmlcalabash/calabash.bat
+++
@@ -1,6 +0,0 @@
-@ehco off
-rem To be able to to use the calabash command from any folder,
-rem change this path to reflect the absolute path of calabash.jar and put it in a folder that 
-rem is located in the path environment variable, such as C:\Windows\System32 or any other folder in the path
-@java -Xmx1024m -jar "C:\Users\username\jats2epub\programs\xmlcalabash\calabash.jar" %*
-    
