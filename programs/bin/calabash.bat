50602315f52a6c17ba43d7348692fcfe46af2088
Eirik Hanssen <eirikh@hioa.no>
2015-10-22 10:54:56 +0200

    update install info on third party programs

--- programs/bin/calabash.bat
+++ programs/bin/calabash.bat
@@ -5,4 +5,4 @@ rem	This is a windows batch file used to run xmlcalabash, an XProc processor
 rem	It is used by the jats2epub.bat windows batch script that should also be
 rem	included
 rem
-@java -jar "%bin%\..\xmlcalabash\calabash.jar" %*
+@java -jar "%bin%\..\xmlcalabash\xmlcalabash.jar" %*
